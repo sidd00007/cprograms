5636656c4bc699d8eacf58f9b1c7fb9ff8f7b2f1
myclass <siddharthachoubey5@gmail.com>
2018-10-05 13:15:53 +0530

    sidd

---
+++ rr1.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+int i;
+printf("enter the week name:");
+scanf("%d",&i);
+switch(i)
+{ case 1:
+        printf("monday");
+break;
+case 2:
+        printf("tuesday");
+break;
+case 3:
+        printf("wednesday");
+break;
+case 4:
+        printf("thrushday");
+break;
+case 5: 
+        printf("friday");
+break;
+case 6:
+        printf("saturday");
+break;
+case 7: 
+        printf("sunday");
+break;
+}
+ return 0;     
+  }
